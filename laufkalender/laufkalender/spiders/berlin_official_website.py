89fcbf295372dfb3d360811805943b45d0071755
Tainara Palmeira <tainarapalmeirag@gmail.com>
2024-07-29 19:13:24 +0200

    refactor: Change spider name from 'events' to 'berlin_official' and add BerlinOfficialItem

--- laufkalender/laufkalender/spiders/berlin_official_website.py
+++ laufkalender/laufkalender/spiders/berlin_official_website.py
@@ -1,13 +1,21 @@
-from typing import Any
 from scrapy import Spider, Request
-from scrapy.http import Response
 
 
 class EventSpider(Spider):
     name = "berlin_official"
-    start_urls = [
-        "https://www.berlin.de/special/sport-und-fitness/laufkalender/"
-    ]
+    start_urls = ["https://www.berlin.de/special/sport-und-fitness/laufkalender/"]
+
+    def parse_event(self, response):
+        event_block = response.xpath("//div[@class='block infocontainer']/dl")
+
+        event_fields = event_block.xpath("./dt/text()").getall()
+        event_values = event_block.xpath("./dd").getall()
+
+        event_data = {
+            event_fields[i]: event_values[i] for i in range(len(event_fields))
+        }
+
+        yield event_data
 
     def parse(self, response):
         events = response.css("div.inner p.text")
@@ -15,7 +23,7 @@ class EventSpider(Spider):
             event_link = event.css("a::attr(href)").get()
             yield response.follow(
                 url=event_link,
-                callback=self.get_event_details,
+                callback=self.parse_event,
             )
         next_page = response.xpath(
             "//nav[@class='pagination ']/ul//li//a[@class='active']/parent::*/following-sibling::li//a/@href"
@@ -24,18 +32,3 @@ class EventSpider(Spider):
         if next_page is not None:
             next_page = response.urljoin(next_page)
             yield Request(next_page, callback=self.parse)
-
-    def get_event_details(self, response):
-        event_details_blocks = response.xpath("//div[@class='block infocontainer']/dl")
-        for event_details_block in event_details_blocks:
-            event_details_keys = event_details_block.xpath("./dt/text()").getall()
-            event_details_values = event_details_block.xpath("./dd/text()").getall()
-
-            print(f"'{event_details_keys}': event_details_values")
-
-            event_data = {
-                event_details_keys[i]: event_details_values[i]
-                for i in range(len(event_details_keys))
-            }
-
-            yield event_data
