4e92b8d21bd5a1b044acf003ce4f3fc55e14705d
Tainara Palmeira <tainarapalmeirag@gmail.com>
2024-12-10 15:51:21 +0100

    Refactor: break down the process_item function into smaller functions to enhance readability.

--- laufkalender/laufkalender/pipelines.py
+++ laufkalender/laufkalender/pipelines.py
@@ -1,28 +1,63 @@
+from scrapy.exceptions import DropItem
 from bs4 import BeautifulSoup
 from datetime import datetime
+import dateparser
 import logging
 import requests
-import json
 
 
 class LaufkalenderPipeline:
-    # support function to runnersworld spider items
-    def convert_event_dates_to_string(self, item):
-        return ", ".join(item["eventdate"])
+    def __init__(self):
+        self.api_url = f"http://localhost:8000/event/"
 
-    # support function to berlin_official spider items
+    def parse_and_format_date(self, eventdate_item):
+        parsed_date = dateparser.parse(eventdate_item)
+        if parsed_date:
+            return parsed_date.strftime("%d/%m/%Y")
+
+    def convert_event_dates_to_string(self, eventdate, spider):
+        try:
+            if isinstance(eventdate, list):
+                parsed_date_list = [
+                    self.parse_and_format_date(eventdate_item)
+                    for eventdate_item in eventdate
+                ]
+                parsed_date_str = ",".join(parsed_date_list)
+                return parsed_date_str
+            elif isinstance(eventdate, str):
+                return self.parse_and_format_date(eventdate)
+            else:
+                raise TypeError(
+                    f"Unsupported type for eventdate: {eventdate} - {spider}"
+                )
+        except Exception as e:
+            logging.exception(f"Error converting event dates: {e} - {spider}")
+            return None
+
+    # Support function for berlin_official to retrieve content and remove HTML tags.
     def clean_data(self, data):
         if not isinstance(data, str):
             return data
         soup = BeautifulSoup(data, "html.parser")
         return soup.get_text()
 
-    def process_item(self, item, spider):
-        url = f"http://localhost:8000/create_event/"
-        if spider.name == "berlin_official":
-            event = {}
+    # Support function to dynamically structure data from the berlin_official spider into standardized fields.
+    def map_fields(self, item, field_mapping):
+        mapped_event = {}
 
-            event_item_fields = {
+        for key, possible_fields in field_mapping.items():
+            for field in possible_fields:
+                if field in item:
+                    mapped_event[key] = self.clean_data(item[field])
+                    break
+            else:
+                mapped_event[key] = ""
+
+        return mapped_event
+
+    def create_event_payload(self, item, spider):
+        if spider.name == "berlin_official":
+            field_mapping = {
                 "title": ["Event", "Laufevent", "Laufkalender"],
                 "description": [],
                 "url": [],
@@ -34,24 +69,21 @@ class LaufkalenderPipeline:
                 "distances": ["Strecke", "Streckenverlauf"],
                 "fee": ["Startgebühr", "Eintritt"],
             }
-            event_item_keys = list(event_item_fields.keys())
+            event = self.map_fields(item, field_mapping)
 
-            for key in event_item_keys:
-                for field in item:
-                    if field in event_item_fields[key]:
-                        data = self.clean_data(item[field])
-                        event[key] = data
-                        break
-                    elif field not in event_item_fields[key]:
-                        event[key] = ""
-            if event["title"]:
-                event.update(
-                    scraped_at=datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-                    spider="berlin_official",
-                )
+            if not event.get("title"):
+                raise DropItem("From Berlin_official spider: Missing event title")
 
-        if spider.name == "runnersworld":
+            event["eventdate"] = self.convert_event_dates_to_string(
+                event["eventdate"], spider.name
+            )
+            event.update(
+                scraped_at=datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                spider="berlin_official",
+            )
+            return event
 
+        elif spider.name == "runnersworld":
             event = {
                 "title": item["title"],
                 "description": item["description"],
@@ -60,17 +92,32 @@ class LaufkalenderPipeline:
                 "address": item["address"],
                 "mailorganizer": item["mailorganizer"],
                 "urlorganizer": item["urlorganizer"],
-                "eventdate": self.convert_event_dates_to_string(item),
-                "distances": str(item["distances"][0]),
-                "fee": "",  # Temporário
+                "eventdate": self.convert_event_dates_to_string(
+                    item["eventdate"], spider.name
+                ),
+                "distances": str(item["distances"]),
+                "fee": "",
                 "scraped_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
                 "spider": "runnersworld",
             }
+            return event
+        else:
+            logging.warning(f"Unknown spider: {spider.name}")
+            return None
 
-        payload_json = json.dumps(event)
+    def post_to_api(self, payload):
         try:
-            response = requests.post(url, data=payload_json)
+            response = requests.post(self.api_url, json=payload)
             response.raise_for_status()
         except requests.RequestException as e:
-            logging.exception(f"Resquest failed: {e}\nFrom event({payload_json.title})")
-        return
+            logging.error(
+                f"Failed to post event: {payload.get('title', 'Unknown')} | Error: {e}"
+            )
+
+    def process_item(self, item, spider):
+        try:
+            payload = self.create_event_payload(item, spider)
+            if payload:
+                self.post_to_api(payload)
+        except Exception as e:
+            logging.exception(f"Error processing item: {e}")
